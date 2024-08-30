fa7238c8480d47535b1539444a0795dc380917ca
ap <avipreshel@hotmail.com>
2024-08-30 14:26:27 +0300

    Cleanups

--- BisterLib.UnitTest/BisterSerializerTests.cs
+++ BisterLib.UnitTest/BisterSerializerTests.cs
@@ -19,28 +19,9 @@ namespace BisterLib.UnitTest
         {
             StringBuilderVerbose sb = new StringBuilderVerbose(true);
 
-            //object instance = new ClassWithPrimitivesOnly()
-            //{
-            //    Prop = TestEnum.Two,
-            //    Prop_bool = true,
-            //    Prop_byte = 1,
-            //    Prop_short = 2,
-            //    Prop_int = 3,
-            //    Prop_long = 4,
-            //    Prop_char = 'c',
-            //    Prop_decimal = 5,
-            //    Prop_ulong = 7,
-            //    Prop_sbyte = 9,
-            //    Prop_string = "10 wow nice string dude",
-            //    Prop_uint = 11,
-            //    Prop_ushort = 12,
-            //    Prop_double = 13.33,
-            //    Prop_float = 14.44f
-            //};
-
             object instance = new object();
 
-            BisterSerializer.SerializeClass(sb, string.Empty, "instance", instance.GetType());
+            BisterSerializer.SerializeAnyType(sb, string.Empty, "instance", instance.GetType());
             Assert.IsTrue(sb.Length != 0);
 
         }
